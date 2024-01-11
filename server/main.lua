6f8c368ee7fad55e2724b05a1b8f65fcec2f146e
Molicheu <64894726+Molicheu@users.noreply.github.com>
2024-01-10 22:28:45 -0500

    Minor change

--- server/main.lua
+++ server/main.lua
@@ -20,12 +20,11 @@ RegisterNetEvent('equip:harness', function(source, item)
 end)
 
 RegisterNetEvent('seatbelt:DoHarnessDamage', function(source, hp, data)
-    if not source then return end
     local player = exports.qbx_core:GetPlayer(source)
 
     local harness = exports.ox_inventory:Search(source, 1, 'harness')
 
-    if not player then return end
+    if not source or not player then return end
 
     if hp == 0 then
         exports.ox_inventory:RemoveItem(source, 'harness', 1, data.metadata, data.slot)
