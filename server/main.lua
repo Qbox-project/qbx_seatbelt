5cafe447e708418199d2463c7a951e5a0a5c8ccc
Molicheu <64894726+Molicheu@users.noreply.github.com>
2024-01-10 23:12:45 -0500

    minor changes

--- server/main.lua
+++ server/main.lua
@@ -1,36 +1,37 @@
 exports.qbx_core:CreateUseableItem('harness', function(source, item)
-    if not source then return end
+    local player = exports.qbx_core:GetPlayer(source)
+    if not player then return end
+
     TriggerClientEvent('seatbelt:client:UseHarness', source, item)
 end)
 
 RegisterNetEvent('equip:harness', function(source, item)
     local player = exports.qbx_core:GetPlayer(source)
-
-    if not source or not player then return end
+    if not player then return end
 
     if item.metadata.harnessuses == nil then
         item.metadata.harnessuses = 19
-        exports.ox_inventory:SetMetadata(source, item.slot, item.metadata)
     elseif item.metadata.harnessuses == 1 then
         exports.ox_inventory:RemoveItem(source, 'harness', 1)
-    else
-        item.metadata.harnessuses -= 1
-        exports.ox_inventory:SetMetadata(source, item.slot, item.metadata)
+        return
     end
+
+    item.metadata.harnessuses -= 1
+    exports.ox_inventory:SetMetadata(source, item.slot, item.metadata)
 end)
 
 RegisterNetEvent('seatbelt:DoHarnessDamage', function(source, hp, data)
     local player = exports.qbx_core:GetPlayer(source)
+    if not player then return end
 
     local harness = exports.ox_inventory:Search(source, 1, 'harness')
 
-    if not source or not player then return end
-
     if hp == 0 then
         exports.ox_inventory:RemoveItem(source, 'harness', 1, data.metadata, data.slot)
-    else
-        harness.metadata.harnessuses -= 1
-        exports.ox_inventory:SetMetadata(source, harness.slot, harness.metadata)
-        TriggerClientEvent("hud:client:UpdateHarness", source, harness.metadata.harnessuses)
+        return
     end
+
+    harness.metadata.harnessuses -= 1
+    exports.ox_inventory:SetMetadata(source, harness.slot, harness.metadata)
+    TriggerClientEvent("hud:client:UpdateHarness", source, harness.metadata.harnessuses)
 end)
