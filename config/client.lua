89522c9a9e1614f00cb7dc8e9e6c3b4fb7872a68
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2024-01-15 19:53:08 +0100

    .

--- config/client.lua
+++ config/client.lua
@@ -5,6 +5,6 @@ return {
 	minSpeedBuckled = 160.0, -- Minimum speed to fly through windscreen when seatbelt is on
     harness = {
         disableFlyingThroughWindscreen = true, -- Disable flying through windscreen when harness is on
-        minSpeed = 200.0 -- Minimum speed to fly through windscreen when harness is on
+        minSpeed = 200.0 -- If the above is set to false, minimum speed to fly through windscreen when harness is on
     }
 }
